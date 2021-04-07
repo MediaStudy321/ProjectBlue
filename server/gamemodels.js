9f3e2e8d00fa662804de99fc694dd0d20ac26ddd
usevalue <bercilac@gmail.com>
2021-04-07 18:13:53 -0400

    Initial game models.

---
+++ server/gamemodels.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const ObjectID = require('mongodb').ObjectID;
+
+const statSchema = new mongoose.Schema({
+    max_hp: {
+        type: Number,
+        default: 1
+    }
+});
+
+const characterSchema = new mongoose.Schema({
+    player: {
+        type: ObjectID
+    },
+    name:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    stats: {
+        type: statSchema
+    }
+});
+
+const partySchema = new mongoose.Schema({
+    members: {
+        type: [ObjectID]
+    }
+})
+
+const Character = mongoose.model(characterSchema);
+
+const Party = mongoose.model(partySchema);
+
+
+
+// const monsterSchema = new mongoose.Schema({
+//     name:{
+//         type: String,
+//         require: true,
+//         unique: true
+//     },
+//     stats: {
+//         type: statSchema
+//     }
+// });
+
+
+module.exports = {Character, Party}
